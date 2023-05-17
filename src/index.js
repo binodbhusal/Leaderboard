80594ed1dfa566197b9dfb48046541f79dc7a352
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-05-17 20:06:30 +0100

    Add event listener and called functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './style.css';
+
+import { updateLeaderboard, handleSubmit } from '../modules/leaderboard.js';
+
+updateLeaderboard();
+const btnSubmit = document.getElementById('btnSubmit');
+btnSubmit.addEventListener('submit', () => {
+  handleSubmit();
+  updateLeaderboard();
+});
